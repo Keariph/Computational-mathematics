50f20379b8befacec0eebac541337ac56675b8cb
Keariph <37950312+Keariph@users.noreply.github.com>
2018-07-01 17:05:23 +0700

    Add files via upload

---
+++ Approximation/Approximation/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Approximation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.CH = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridViewApp = new System.Windows.Forms.DataGridView();
+            this.xi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.koef = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewApp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CH
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.CH.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.CH.Legends.Add(legend1);
+            this.CH.Location = new System.Drawing.Point(308, -4);
+            this.CH.Name = "CH";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Approxim";
+            series2.BorderColor = System.Drawing.Color.Black;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.CustomProperties = "IsXAxisQuantitative=False";
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series2.Name = "Point";
+            this.CH.Series.Add(series1);
+            this.CH.Series.Add(series2);
+            this.CH.Size = new System.Drawing.Size(480, 238);
+            this.CH.TabIndex = 0;
+            this.CH.Text = "CH";
+            // 
+            // dataGridViewApp
+            // 
+            this.dataGridViewApp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewApp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.xi,
+            this.yi,
+            this.koef});
+            this.dataGridViewApp.Location = new System.Drawing.Point(0, -4);
+            this.dataGridViewApp.Name = "dataGridViewApp";
+            this.dataGridViewApp.Size = new System.Drawing.Size(302, 238);
+            this.dataGridViewApp.TabIndex = 1;
+            // 
+            // xi
+            // 
+            this.xi.HeaderText = "xi";
+            this.xi.Name = "xi";
+            // 
+            // yi
+            // 
+            this.yi.HeaderText = "yi";
+            this.yi.Name = "yi";
+            // 
+            // koef
+            // 
+            this.koef.HeaderText = "koef";
+            this.koef.Name = "koef";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Степень полинома";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ввод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 303);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewApp);
+            this.Controls.Add(this.CH);
+            this.Name = "Form1";
+            this.Text = "Approx";
+            ((System.ComponentModel.ISupportInitialize)(this.CH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewApp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart CH;
+        private System.Windows.Forms.DataGridView dataGridViewApp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn koef;
+    }
+}
+
---
+++ Approximation/Approximation/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Approximation
+{
+    public partial class Form1 : Form
+    {
+        int N;
+        List<double> x = new List<double> { 0, 1, 2, 3, 4, 5 };
+        List<double> y = new List<double> { 0, 3, 4, 7, 8, 10 };
+        List<double> B = new List<double>();
+        List<double> A = new List<double>();
+        List<List<double>> C = new List<List<double>>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //public void FileRead()//Чтение с файла
+        //{
+        //    string str;
+        //    FileStream file = new FileStream("C:\\Users\\Evil Spirit\\source\\repos\\Gauss\\Approximation\\FileXY.txt", FileMode.Open, FileAccess.Read);
+        //    StreamReader reader = new StreamReader(file);
+        //    str = reader.ReadLine();
+        //    while (str != null) //пока читается
+        //    {
+        //        string[] buf = str.Split(' ');
+        //        x.Add(Convert.ToDouble(buf[0]));
+        //        y.Add(Convert.ToDouble(buf[1]));
+        //        str = reader.ReadLine(); ;
+        //    }
+        //    reader.ReadLine();
+        //}
+        public double G(double x, int i)
+        {
+            double g = 0;
+            switch (i)
+            {
+                case 0:
+                    {
+                        g = 1;
+                        break;
+                    }
+                case 1:
+                    {
+                        g = x;
+                        break;
+                    }
+                case 2:
+                    {
+                       g = Math.Pow(x, 2);
+                        break;
+                    }
+                case 3:
+                    {
+                        g = Math.Pow(x, 3);
+                        break;
+                    }
+                case 4:
+                    {
+                        g = Math.Pow(x, 4);
+                        break;
+                    }
+                case 5:
+                    {
+                        g = Math.Pow(x, 5);
+                        break;
+                    }
+                case 6:
+                    {
+                        g = Math.Pow(x, 6);
+                        break;
+                    }
+                case 7:
+                    {
+                        g = Math.Pow(x, 7);
+                        break;
+                    }
+                case 8:
+                    {
+                        g = Math.Pow(x, 8);
+                        break;
+                    }
+            }
+            return g;
+        }
+        public void MatC()
+        {
+            //for (int i = 0; i < N; i++)
+            //{
+            //    if (N > x.Count)
+            //        N = x.Count;
+            //    C.Add(new List<double>());
+            //    for (int j = 0; j < N; j++)
+            //    {
+            //        C[i].Add(0);
+            //            for (int k = 0; k < x.Count; k++)
+            //            {
+            //                C[i][j] += Math.Pow(x[k], i+j) /*(G(x[k], i) * G(x[k], j))*/;
+            //            }
+            //    }
+            //}
+            if (N > x.Count)
+                N = x.Count;
+            for (int i = 0; i < N; i++)
+            {
+                C.Add(new List<double>());
+                for (int j = 0; j < N; j++)
+                {
+                    C[i].Add(0);
+                    for (int k = 0; k < x.Count; k++)
+                        C[i][j] += Math.Pow(x[k], i + j);
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                B.Add(0);
+                for (int k = 0; k < x.Count; k++)
+                {
+                    B[i] += y[k] * Math.Pow(x[k], i);
+                }
+            }
+        }
+        //public void VecB()
+        //{
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        B.Add(0);
+        //        for (int k = 0; k < x.Count; k++)
+        //        {
+        //            B[i] += y[k] */* G(x[k], i)*/Math.Pow(x[k], i);
+        //        }
+        //    }
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            N = Convert.ToInt32(textBox1.Text);
+            B.Clear();
+            for(int i = 0; i < C.Count; i++)
+            {
+                C[i].Clear();
+            }
+            C.Clear();
+            A.Clear();
+           // FileRead();
+            MatC();
+            //VecB();
+            A = GaussMethodMOD(ref C, ref B);
+            PrintGraphic();
+        }
+        private double Polynom(double x)
+        {
+            double result = 0;
+            for (int i = 0; i < N - 1; i++)
+            {
+                result += A[i] * G(x, i);
+            }
+            return result;
+        }
+        public void PrintGraphic()
+        {
+            CH.Series[0].Points.Clear();
+            CH.Series[1].Points.Clear();
+            CH.ChartAreas[0].AxisX.ScaleView.Zoom(0, x[x.Count - 1]); // -15 <= x <= 2
+            CH.ChartAreas[0].CursorX.IsUserEnabled = true;
+            CH.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            CH.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            CH.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            CH.ChartAreas[0].AxisY.ScaleView.Zoom(0, y[y.Count - 1] ); // -15 <= x <= 2
+            CH.ChartAreas[0].CursorY.IsUserEnabled = true;
+            CH.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            CH.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            CH.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+            for (int i = 0; i < y.Count; i++)
+            {
+                CH.Series[1].Points.AddXY(x[i], y[i]);
+            }
+            for (double i = 0; i < x.Count; i += 0.1)
+            {
+                CH.Series[0].Points.AddXY(i, Polynom(i));
+            }
+            for(int i = 0; i <N;i++)
+            {
+                dataGridViewApp.Rows.Add(x[i], y[i], A[i]);
+            }
+        }
+        private List<double> GaussMethodMOD(ref List<List<double>> Array, ref List<double> VEC)
+        {
+            for (int i = 0; i < VEC.Count; i++)
+            {
+                NormalizeString(ref Array, ref VEC, VEC.Count, i, i);
+                NormalizeKOEF(ref Array, ref VEC, VEC.Count, i);
+                Subtraction(ref Array, ref VEC, i, VEC.Count);
+            }
+            return BackwardSubstitution(ref Array, ref VEC, VEC.Count);
+        }
+        private void NormalizeString(ref List<List<double>> Array,ref List<double> VEC, int size, int k, int m)
+        {
+            double max = Math.Abs(Array[m][k]);
+            int f = -1;
+            int z = m;
+            while (z + 1 < size)
+                if (max < Math.Abs(Array[z + 1][k]))
+                {
+                    max = Array[z + 1][k];
+                    z++;
+                    f = z;
+                }
+                else
+                {
+                    z++;
+                }
+            swapString(ref Array, ref VEC, k, f, size);
+            if (max < 0.00001)
+            {
+                return;
+            }
+        }
+        private void NormalizeKOEF(ref List<List<double>> Array, ref List<double> VEC, int size, int k)
+        {
+            double temp;
+            for (int i = k; i < size; i++)
+            {
+                temp = Array[i][k];
+                if (temp != 0)
+                {
+                    for (int j = k; j < size; j++)
+                    {
+                        Array[i][j] = Array[i][j] / temp;
+                    }
+                    VEC[i] = VEC[i] / temp;
+                }
+            }
+        }
+        private void Subtraction(ref List<List<double>> Array, ref List<double> VEC, int k, int size)
+        {
+            for (int i = k + 1; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Array[i][j] -= Array[k][j];
+                }
+                VEC[i] -= VEC[k];
+            }
+        }
+        private void swapString(ref List<List<double>> Array, ref List<double> VEC, int k, int m, int size)
+        {
+            double buffer;
+            if (m == -1)
+                return;
+            for (int i = 0; i < size; i++)
+            {
+                buffer = Array[k][i];
+                Array[k][i] = Array[m][i];
+                Array[m][i] = buffer;
+            }
+            buffer = VEC[k];
+            VEC[k] = VEC[m];
+            VEC[m] = buffer;
+        }
+        private List<double> BackwardSubstitution(ref List<List<double>> Array, ref List<double> VEC, int size)
+        {
+            int k = 0;
+            List<double> x = new List<double>();
+            for (int i = size - 1; i >= 0; i--)
+            {
+                x.Add(0);
+                x[k] = VEC[i];
+                for (int j = 0; j < i; j++)
+                    VEC[j] = VEC[j] - Array[j][i] * x[k];
+                k++;
+            }
+            return x;
+        }
+    }
+}
